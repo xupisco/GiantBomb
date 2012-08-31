d26419f391eef19f9f5ef6755ad9ed59511725d7
f4nt <f4nt@f4ntasmic.com>
2012-08-31 00:05:38 -0500

    added image urls to search results

--- giantbomb/giantbomb.py
+++ giantbomb/giantbomb.py
@@ -40,7 +40,7 @@ class Api:
             raise GiantBombError('Error code %s: %s' % (resp['status_code'], resp['error']))
 
     def search(self, query, offset=0):
-        results = simplejson.load(urllib2.urlopen(self.base_url + "/search/?api_key=%s&resources=game&query=%s&field_list=id,name&offset=%s&format=json" % (self.api_key, urllib2.quote(query), offset)))
+        results = simplejson.load(urllib2.urlopen(self.base_url + "/search/?api_key=%s&resources=game&query=%s&field_list=id,name,image&offset=%s&format=json" % (self.api_key, urllib2.quote(query), offset)))
         return [SearchResult.NewFromJsonDict(x) for x in self.checkResponse(results)]
 
     def getGame(self, id):
@@ -269,18 +269,22 @@ class SearchResult:
     def __init__(self,
                  id=None,
                  name=None,
-                 api_detail_url=None):
+                 api_detail_url=None,
+                 image=None):
 
         self.id = id
         self.name = name
         self.api_detail_url = api_detail_url
+        self.image = image
 
     @staticmethod
     def NewFromJsonDict(data):
+        print data
         if data:
             return SearchResult(id=data.get('id'),
                                 name=data.get('name', None),
-                                api_detail_url=data.get('api_detail_url', None))
+                                api_detail_url=data.get('api_detail_url', None),
+                                image=data.get('image', None))
         return None
 
     def __repr__(self):
